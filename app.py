9bb18276e33195fdc5152d3bfb106cb079626cae
Adwaith PJ <adwaithleans616@gmail.com>
2024-03-23 02:01:25 +0530

    Main executing file - dont run for now

---
+++ app.py
@@ -0,0 +1,21 @@
+from encoder_cpu_pool import Encoder1
+from encoder import Encoder
+from faceRecognizer import FaceRecognition
+import time
+
+if __name__ == "__main__":
+    # start_time = time.time()
+    # encoder = Encoder1()
+    # encoder.load_images()
+    # encoder.findEncoding(encoder.imgList)
+    # encoder.pickle_data('test_3')
+    # end_time = time.time()
+    #
+    # elapsed_time = end_time - start_time
+    # print(elapsed_time)
+    #
+    start_time = time.time()
+    reco =FaceRecognition('test_2')
+    reco.recognize_face()
+    end_time = time.time()
+    print(start_time - end_time)
