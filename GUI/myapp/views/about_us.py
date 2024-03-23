4797bd2accaec6dab22379e5414dfb32aa78a3d6
Adwaith PJ <adwaithleans616@gmail.com>
2024-03-23 14:22:26 +0530

    Unwanted Files removed

--- GUI/myapp/views/about_us.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-from flet_core import border_radius, margin , padding, Animation,page
-from flet_route import Params, Basket
-
-
-class AboutUs:
-    def __init__(self):
-        self.about_us_full_page =
